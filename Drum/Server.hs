6aef55e52f3c99dae3c40abbb3062e30b03f6e3e
alex <alex@slab.org>
2018-06-28 11:34:27 +0100

    much stuff

--- Drum/Server.hs
+++ Drum/Server.hs
@@ -17,6 +17,9 @@ import Control.Concurrent.MVar
 import System.Directory
 import System.FilePath
 import System.IO
+import Control.Monad (when)
+import Control.Monad.Trans (liftIO)
+import Data.Unique
 
 -- import Data.Ratio
 -- import System.Process
@@ -31,7 +34,9 @@ port = 6010
 data Client = Client {cName :: String,
                       cServerThread :: ThreadId,
                       cSender :: String -> IO (),
-                      cFH :: Maybe Handle
+                      cFH :: Maybe Handle,
+                      cConn :: WS.Connection,
+                      cId :: Unique
                      }
 
 wsSend :: WS.Connection -> IO (ThreadId, String -> IO())
@@ -48,6 +53,7 @@ wsSend conn =
 run = do
   putStrLn $ "Drumming circle server, starting on port " ++ show port
   mConnectionId <- newMVar 0
+  mClients <- newMVar []
   WS.runServer "0.0.0.0" port $ (\pending -> do
     conn <- WS.acceptRequest pending
 
@@ -57,32 +63,49 @@ run = do
     sender $ "/welcome"
     
     WS.forkPingThread conn 30
-
-    let clientState = Client {cName = "anonymous",
-                              cServerThread = senderThreadId,
-                              cSender = sender,
-                              cFH = Nothing
-                             }
-    loop clientState conn
+    uniqueId <- liftIO newUnique
+    let client = Client {cName = "anonymous",
+                         cServerThread = senderThreadId,
+                         cSender = sender,
+                         cFH = Nothing,
+                         cConn = conn,
+                         cId = uniqueId
+                        }
+    clients <- liftIO $ takeMVar mClients
+    liftIO $ putMVar mClients $ client:clients
+    loop mClients client conn
     )
   putStrLn "done."
 
-loop :: Client -> WS.Connection -> IO ()
-loop clientState conn = do
+loop :: MVar [Client] -> Client -> WS.Connection -> IO ()
+loop mClients client conn = do
   msg <- try (WS.receiveData conn)
   -- add to dictionary of connections -> patterns, could use a map for this
   case msg of
     Right s -> do
       putStrLn $ "msg: " ++ T.unpack s
-      clientState' <- act clientState conn (T.unpack s)
-      loop clientState' conn
-    Left WS.ConnectionClosed   -> close clientState "unexpected loss of connection"
-    Left (WS.CloseRequest _ _) -> close clientState "by request from peer"
-    Left (WS.ParseException e) -> close clientState ("parse exception: " ++ e)
+      client' <- act mClients client conn (T.unpack s)
+      clients <- takeMVar mClients
+      putMVar mClients $ updateClient clients client'
+      loop mClients client' conn
+    Left WS.ConnectionClosed   -> close mClients client "unexpected loss of connection"
+    Left (WS.CloseRequest _ _) -> close mClients client "by request from peer"
+    Left (WS.ParseException e) -> close mClients client ("parse exception: " ++ e)
 
-close :: Client -> String -> IO ()
-close clientState msg = do
+removeClient :: [Client] -> Client -> [Client]
+removeClient cs c = filter (\c' -> cId c' /= cId c) cs
+
+updateClient :: [Client] -> Client -> [Client]
+updateClient cs c = c:(removeClient cs c)
+
+close :: MVar [Client] -> Client -> String -> IO ()
+close mClients client msg = do
   putStrLn ("connection closed: " ++ msg)
+  let fh = cFH client
+  cs <- takeMVar mClients
+  putMVar mClients $ removeClient cs client
+  when (isJust fh) $ do
+    hClose $ fromJust fh
 -- hush = mapM_ ($ Tidal.silence)
 
 -- TODO: proper parsing..
@@ -91,7 +114,9 @@ takeNumbers xs = (takeWhile f xs, dropWhile (== ' ') $ dropWhile f xs)
   where f x = not . null $ filter (x ==) "0123456789."
 
 commands = [("name", act_name),
-            ("change", act_change)
+            ("change", act_change),
+            ("takeSnapshots", act_takeSnapshots),
+            ("replay", act_replay)
            {-("play", act_play),
             ("record", act_record),
             ("typecheck", act_typecheck) ,
@@ -107,7 +132,7 @@ commands = [("name", act_name),
             ("nobang", act_bang False)-}
            ]
 
-getCommand :: String -> Maybe (Client -> WS.Connection -> IO Client)
+getCommand :: String -> Maybe (MVar [Client] -> Client -> WS.Connection -> IO Client)
 getCommand ('/':s) = do f <- lookup command commands
                         param <- stripPrefix command s
                         let param' = dropWhile (== ' ') param
@@ -115,29 +140,48 @@ getCommand ('/':s) = do f <- lookup command commands
   where command = takeWhile (/= ' ') s
 getCommand _ = Nothing
 
-act :: Client -> WS.Connection -> String -> IO Client
-act clientState conn request = (fromMaybe act_no_parse $ getCommand request) clientState conn
+act :: MVar [Client] -> Client -> WS.Connection -> String -> IO Client
+act mClients client conn request = (fromMaybe act_no_parse $ getCommand request) mClients client conn
 
-act_no_parse clientState conn = do cSender clientState $ "/noparse"
-                                   return clientState
+act_no_parse _ client conn = do cSender client $ "/noparse"
+                                return client
 
 
-act_name :: String -> Client -> WS.Connection -> IO (Client)
-act_name param clientState conn =
+act_name :: String -> MVar [Client] -> Client -> WS.Connection -> IO (Client)
+act_name param _ client conn =
   do putStrLn $ "name: '" ++ param ++ "'"
-     cSender clientState $ "/name ok hello " ++ param
-     return $ clientState {cName = param}
+     cSender client $ "/name ok hello " ++ param
+     return $ client {cName = param}
 
-act_change :: String -> Client -> WS.Connection -> IO (Client)
-act_change param clientState conn =
-  do fh <- getFH (cFH clientState)
+act_change :: String -> MVar [Client] -> Client -> WS.Connection -> IO (Client)
+act_change param _ client conn =
+  do fh <- getFH (cFH client)
      hPutStrLn fh $ "//"
      hPutStrLn fh param
      hFlush fh
-     return (clientState {cFH = Just fh})
+     return (client {cFH = Just fh})
        where getFH (Just fh) = return fh
              getFH Nothing =
                do createDirectoryIfMissing True logDirectory
-                  openFile fn AppendMode
-             fn = "logs" </> (cName clientState) ++ ".json"
+                  fh <- openFile fn AppendMode
+                  hPutStrLn fh $ "// connect"
+                  return fh
+             fn = "logs" </> (cName client) ++ ".json"
              logDirectory = "logs"
+
+
+act_takeSnapshots :: String -> MVar [Client] -> Client -> WS.Connection -> IO (Client)
+act_takeSnapshots param mClients client conn =
+  do putStrLn $ "takeSnapshot [" ++ param ++ "]."
+     cs <- readMVar mClients
+     mapM_ (\conn -> WS.sendTextData conn (T.pack $ "/takeSnapshot " ++ param)) $ map cConn cs
+     return $ client
+
+
+act_replay :: String -> MVar [Client] -> Client -> WS.Connection -> IO (Client)
+act_replay param mClients client conn =
+  do putStrLn $ "replay [" ++ param ++ "]."
+     cs <- readMVar mClients
+     mapM_ (\conn -> WS.sendTextData conn (T.pack $ "/replay " ++ param)) $ map cConn cs
+     return $ client
+
